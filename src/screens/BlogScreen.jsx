4c16d66e775c91e5b6c08e9ba24b0e32070ec48a
Ian-Mark <ianarapkirui@gmail.com>
2023-02-02 17:01:03 +0300

    publications screen ui fixes

--- src/screens/BlogScreen.jsx
+++ src/screens/BlogScreen.jsx
@@ -1,28 +1,44 @@
-import React from 'react';
-import { blogList } from '../components/Blogs/Data';
-import BlogHeader from '../components/Blogs/BlogHeader';
-import TopNavbar from '../components/Nav/TopNavbar';
-import BlogIndex from '../components/Blogs/BlogIndex';
+import React from "react";
+import "./BlogScreen.css";
+import { blogList } from "../components/Blogs/Data";
+import BlogHeader from "../components/Blogs/BlogHeader";
+import TopNavbar from "../components/Nav/TopNavbar";
+import BlogIndex from "../components/Blogs/BlogIndex";
 
 const BlogScreen = () => {
-    return (
-        <>
-        <TopNavbar />
-        <BlogHeader/>
-        <div className='blog-container'>
-            <div className='blog-flex'>
-            <BlogIndex/>
-            </div>
-            <div className='sidebar-flex'>
-                Categories
-            </div>
-            
+  return (
+    <>
+      <TopNavbar />
+      <BlogHeader />
+      <div className="blog-container">
+        <div className="blog-flex">
+          <BlogIndex />
         </div>
-        {/* <AllBlogs/> */}
-        {/* <Blogs/> */}
-        
-        </>
-    );
-}
+        <div className="sidebar-flex">
+          <div className="category-card">
+            <div className="top">Categories</div>
+            <hr style={{ height: "1px", width: "100%" }} />
+            <div className="bottom">
+              <div className="categories">
+                <span>crime</span>
+                <span>10</span>
+              </div>
+              <div className="categories">
+                <span>crime</span>
+                <span>10</span>
+              </div>
+              <div className="categories">
+                <span>crime</span>
+                <span>10</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <AllBlogs/> */}
+      {/* <Blogs/> */}
+    </>
+  );
+};
 
 export default BlogScreen;
