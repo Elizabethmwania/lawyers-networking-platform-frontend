fe2f91c0f690969eba036c2fab8a3349e255ee5a
Elizabethmwania <elizabethmwania257@gmail.com>
2023-02-13 18:02:54 +0300

    scroll button

--- src/components/Elements/Backdrop.jsx
+++ src/components/Elements/Backdrop.jsx
@@ -1,31 +1,20 @@
 import React, { useEffect } from "react";
-import styled from "styled-components";
-import { ArrowUpCircleIcon } from "@heroicons/react/24/solid";
+import ScrollToTop from "react-scroll-to-top";
 export default function Backdrop() {
   useEffect(() => {
     window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
   }, []);
 
   return (
-    <div>
-      <button
-        onClick={() => {
-          window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-        }}
-        style={{
-          position: 'fixed',
-          bottom: '20px',
-          right: '40px',
-          color: '#d49733',
-          width: '50px',
-          height: '50px',
-          backgroundColor:'transparent',
-          border: 'none'
-        }}
-      >
-        <ArrowUpCircleIcon />
-      </button>
-    </div>
+      <ScrollToTop
+      smooth
+      style={{
+        bottom: '20px',
+        width: '50px',
+        height: '50px',
+        backgroundColor:'#d49733',
+      }}
+      />
   )
 }
 
