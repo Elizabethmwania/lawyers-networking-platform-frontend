d71fa2d39b973f2cede748bdce67e48494fc3463
Elizabethmwania <elizabethmwania257@gmail.com>
2023-03-04 20:55:54 +0300

    bug fix-dashboard

--- src/screens/DashboardScreen.jsx
+++ src/screens/DashboardScreen.jsx
@@ -252,10 +252,10 @@ function DashboardScreen() {
               </div>
             </div>
           </div>
-          <br />
+          {/* <br /> */}
           {/* Reports */}
           <div className="row">
-            <div className="col-md-6">
+            <div className="col-md-6" style={{marginBottom:"25px"}}>
               <div className="card card-table flex-fill">
                 <div className="card-header">
                   <h4 className="card-titles">Briefs Analytics #1</h4>
@@ -283,7 +283,7 @@ function DashboardScreen() {
                 </div>
               </div>
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6" style={{marginBottom:"25px"}}>
               <div className="card  card-table flex-fill">
                 <div className="card-header">
                   <h4 className="card-titles">Briefs Analytics #2</h4>
